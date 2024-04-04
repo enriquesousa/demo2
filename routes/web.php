52bc6d55fa75e2088570666141c3f38829b5f9fd
Enrique <enrique.sousa@gmail.com>
2024-04-03 17:12:17 -0700

    Routes test1 y test2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test1', function () {
+    return view('test1');
+});
+
+Route::get('/test2', function () {
+    return view('test2');
+});
